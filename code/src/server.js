3ed366dee6badce5e7ce06ee8552a93b94b2412b
Guille A <LGuille.2000@gmail.com>
2023-03-22 21:29:04 -0300

    socketAndSee

---
+++ code/src/server.js
@@ -0,0 +1,38 @@
+import express from "express"
+import handlebars from "express-handlebars"
+import { Server } from "socket.io"
+import {__dirname} from "./utils/utils.js"
+import productsRouter from "./routes/routerProducts/routerProducts.js"
+import cartsRouter from './routes/routerCarts/routerCarts.js'
+import realTimeProducts, {ioServer} from "./routes/realtimeproducts/realtimeproducts.js"
+
+
+//variables
+const app = express()
+const PORT = process.env.PORT || 8080
+
+//appUse
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
+console.log(__dirname)
+
+//handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+//routes
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+app.use('/api/realtimeproducts', realTimeProducts)
+
+//Server Run
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server HTTP run in PORT ${PORT}`)
+})
+
+//server socket
+const socketServer = new Server(httpServer)
+//export server
+ioServer(socketServer)
