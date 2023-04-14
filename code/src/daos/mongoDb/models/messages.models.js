9927b639d3159dd0dc7b9e2609256815ef470cc9
Guille A <LGuille.2000@gmail.com>
2023-04-14 20:04:05 -0300

    complementaryChallenge

--- code/src/daos/mongoDb/models/messages.models.js
+++ code/src/daos/mongoDb/models/messages.models.js
@@ -1,8 +1,8 @@
 import { Schema, model } from "mongoose"
 
 const messagesSchema = new Schema({
-    user: {type: String, require: true},
     message: [{type: String, require: true}],
+    user: {type: String, require: true},
     fyh: {type: Date, default: Date.now}
 })
 
