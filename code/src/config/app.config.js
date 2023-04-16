fc02896f1f3f3298f773f01857f052472126634c
Guille A <LGuille.2000@gmail.com>
2023-04-16 10:05:15 -0300

    update complementaryChallenge

---
+++ code/src/config/app.config.js
@@ -0,0 +1,18 @@
+import handlebars from "express-handlebars"
+import { router } from '../routes/index.js'
+import {__dirname} from "../utils/utils.js"
+
+
+export const appConfig = async (app, express) => {
+    // config
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.static(__dirname + '/public'))
+    //handlebars
+    app.engine('handlebars', handlebars.engine())
+    app.set('views', __dirname + '/views')
+    app.set('view engine', 'handlebars')
+
+    //routes
+    app.use(router)
+}
