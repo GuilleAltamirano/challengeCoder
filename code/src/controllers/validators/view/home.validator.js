65448318ab140441666e925cd883b1b18de1d06a
Guille A <LGuille.2000@gmail.com>
2023-04-22 22:05:54 -0300

    add nav and footer

--- code/src/controllers/validators/view/home.validator.js
+++ code/src/controllers/validators/view/home.validator.js
@@ -1,6 +1,10 @@
 import { productsServices } from "../../../daos/mongoDb/services/Products.services.js"
 
 export const homeValidator = async(query) => {
+    //var
+    const arrayPage = []
+    let prevLink = '#'
+    let nextLink = '#'
     //destructuring query
     const { page=1, limit=3, category, sort } = query
     //destructuring paginate
@@ -11,6 +15,14 @@ export const homeValidator = async(query) => {
         hasNextPage,
         nextPage, 
     } = await productsServices.productsPaginate({ page, limit, category, sort })
+    //for navigation button
+    for (let i = 1; i <= totalPages; i++) {
+        arrayPage.push(i)
+    }
+    //prev link
+    if (hasPrevPage) prevLink = `http://localhost:8080/?page=${prevPage}`
+    if (hasNextPage) nextLink = `http://localhost:8080/?page=${nextPage}`
+
     //return
     return {
         status: true,
@@ -19,7 +31,10 @@ export const homeValidator = async(query) => {
         hasPrevPage,
         prevPage,
         hasNextPage,
-        nextPage
+        nextPage,
+        arrayPage,
+        prevLink,
+        nextLink
     }
 
 }
