1db6e4c65a9b0fcbf574d2bcce83ff5bd43bb432
Guille A <LGuille.2000@gmail.com>
2023-06-26 19:40:41 -0300

    logger

--- src/config/socket.config.js
+++
@@ -1,8 +0,0 @@
-import { Server } from "socket.io"
-
-export const socketConfig = async (httpServer) => {
-    try {
-        const io = new Server(httpServer)
-        console.info('Socket server run')
-    } catch (err) {console.error(err)}
-}
--- src/dao/DTOs/carts.dto.js
+++
@@ -1,9 +0,0 @@
-export class PurchaseDto {
-    constructor (data) {
-        this._id = data.product._id
-        this.stock = data.product.stock
-        this.price = data.product.price
-        this.status = data.product.status
-        this.quantity = data.quantity
-    }
-}
