ed08ae5ebe3d43804879cb04741b4de27e75c980
Guille A <LGuille.2000@gmail.com>
2023-06-26 22:54:32 -0300

    logger

--- src/server/main.js
+++ src/server/main.js
@@ -4,10 +4,12 @@ import { socketConfig } from './config/socket.config.js'
 import varsEnv from './env/vars.env.js'
 import ViteExpress from "vite-express"
 import { logger } from './utils/logger.js'
+import {cpus} from 'os'
 
 const app = express()
 await appConfig(app, express)
 
+logger.info(cpus().length)
 
 const httpServer = app.listen(varsEnv.PORT, () => {
   logger.info(`Server http run in route localhost:${varsEnv.PORT} ✅`)
